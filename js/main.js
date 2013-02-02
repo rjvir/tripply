0f9e85292abfc8e20969d6eb3be441147e9806d7
Jesse Daugherty <jessedaugherty@gmail.com>
2013-02-02 05:25:07 -0500

    js is pulling for a hardcoded city

--- js/main.js
+++ js/main.js
@@ -5,32 +5,34 @@ Parse.initialize("mfn8KBuLDmeUenYE1VGUYQr2x5YDFJQ669TZ7HSL", "nMBVdpIpZ3XjGMBMOy
 
 var TRIP = TRIP || {};
 $.extend(TRIP, {
-	user_airport,
-
+	user_airport: "ATL",
 	appendDeal: function(flight, styleName) {
 		//create HTML OBJ
-		var HTML0 = "<div class=deal><div class=deal_destination>",
-			HTML1 = "</div><div class=deal_days>",
-			HTML2 = "</div><div class=deal_price>",
-			HTML3 = "</div></div>",
-			numDays = flight.get("end_data") - flight.get("start_date");
+		var HTML0 = "<div class=deal><div class=destLocation> Location: ",
+			HTML1 = "</div><div class=deal_days> Nights: ",
+			HTML2 = "</div><div class=deal_price> Price : ",
+			HTML3 = "</div></div>";
+			var startDate = flight.get("departDate").replace(/\//g,""),
+			endDate = flight.get("returnDate").replace(/\//g,"");
+			startDate = parseInt(startDate);
+			endDate = parseInt(endDate);
+			var numDays = (endDate - startDate) / 100;
 		$('.deals').append(
-  			HTML0 + flight.get("destination") +
+  			HTML0 + flight.get("destLocation") +
   			HTML1 + numDays +
-  			HTML2 + flight.get("price") + HTML3;
+  			HTML2 + flight.get("price") + HTML3
 	  	);
-	},
-	handleDeals: function(deals) {
-		
 	},
 	getDeals: function() {
 		var Deals = Parse.Object.extend("Deals"),
 		deal = new Deals(),
 		query = new Parse.Query(Deals);
-		query.equalTo("origin", TRIP.user_airport);
+		query.equalTo("originCode", TRIP.user_airport);
 		query.find({
 		  success: function(results) {
-		  	TRIP.handleDeals(results);
+		  	$.each(results, function() {
+		  		TRIP.appendDeal(this);
+		  	});
 		  },
 		  error: function(error) {
 		    alert("Error: " + error.code + " " + error.message);
@@ -41,5 +43,5 @@ $.extend(TRIP, {
 
 $(function() {
      // Same as $(document).ready(function {}). TIL
-     SHIP.getDeals();
+     TRIP.getDeals();
 });
