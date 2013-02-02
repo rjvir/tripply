f3cd5b646bafab4b6d295fabbda9a0168641b362
Pratik Kabra <pratik@kabra.com>
2013-02-02 02:01:24 -0500

    added magpie

--- flightfinder/flightfinder.php
+++ flightfinder/flightfinder.php
@@ -7,9 +7,11 @@ if(($file = fopen("airports.csv","r")) !== false){
 	}
 }
 else die('unable to get companies');
-echo $airports[0];
 
-echo $xml = simplexml_load_file("http://www.kayak.com/h/rss/buzz?code=".$airports[0]);
+$xml = file_get_contents("http://www.kayak.com/h/rss/buzz?code=".$airports[0]);
+print_r($xml);
+
+
 /*
 foreach($airports as $airport){
 	$xml = simplexml_load_file("http://www.kayak.com/h/rss/buzz?code=".$airport);
