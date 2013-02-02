3e7b1b4c851d520d4db14ead5d182f63e8cee899
Pratik Kabra <pratik@kabra.com>
2013-02-02 02:27:23 -0500

    parsed data xml to json

--- flightfinder/flightfinder.php
+++ flightfinder/flightfinder.php
@@ -1,5 +1,17 @@
 <?php
 
+function parse($url) {
+		$fileContents= file_get_contents($url);
+		$fileContents = str_replace(array("\n", "\r", "\t","kyk:"), '', $fileContents);
+		$fileContents = trim(str_replace('"', "'", $fileContents));
+		$simpleXml = simplexml_load_string($fileContents);
+		$json = json_encode($simpleXml, JSON_PRETTY_PRINT);
+		$json = json_decode($json);
+		$json = $json->channel->item;
+		$json = json_encode($json, JSON_PRETTY_PRINT);
+		return $json;
+}
+
 $airports = array();
 if(($file = fopen("airports.csv","r")) !== false){
 	while($data = fgetcsv($file)){
@@ -8,8 +20,11 @@ if(($file = fopen("airports.csv","r")) !== false){
 }
 else die('unable to get companies');
 
-$xml = file_get_contents("http://www.kayak.com/h/rss/buzz?code=".$airports[0]);
-print_r($xml);
+$rss = parse("http://www.kayak.com/h/rss/buzz?code=".$airports[0]."&tm=".date("Ym"));
+
+echo "<pre>";
+echo $rss;
+echo "</pre>";
 
 
 /*
