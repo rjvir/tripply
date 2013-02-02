accdc19bd45cf596ca481d9e8a2724bcab6bc355
Jesse Daugherty <jessedaugherty@gmail.com>
2013-02-02 08:31:16 -0500

    4 columns

--- js/main.js
+++ js/main.js
@@ -6,20 +6,33 @@ Parse.initialize("mfn8KBuLDmeUenYE1VGUYQr2x5YDFJQ669TZ7HSL", "nMBVdpIpZ3XjGMBMOy
 var TRIP = TRIP || {};
 $.extend(TRIP, {
 	user_airport: "DTW",
+	parseDate: function(str) {
+		var mdy = str.split('/')
+    	return new Date('20' + mdy[2], mdy[0]-1, mdy[1]);
+	},
+	daydiff: function(start, end) {
+		return Math.round((end-start)/(1000*60*60*24));
+	},
+	locationString: function(str) {
+		var parts = str.split(",");
+		if (parts) {
+			if (parts[1] != " DC") return parts[0];
+		}
+		return str;
+	},
 	appendDeal: function(flight, styleName) {
-      	var imgUrl = "http://www.travelwizard.com/fiji/media/wadigibeach.jpg",
-      		HTML0 = "<div class='deal-box grid_4'><div class=border-wrapper><div class=deal style='background-image:url(",
+      	var imgUrl = flight.get("destImage"),
+      		HTML0 = "<div class='deal-box grid_3'><div class=border-wrapper><div class=deal style='background-image:url(",
 			HTML1 = ");'> <div class=destination>",
 			HTML2 = "<br /><div class=price>",
-			HTML3 = "</div></div></div></div></div>";
-			var startDate = flight.get("departDate").replace(/\//g,""),
-			endDate = flight.get("returnDate").replace(/\//g,"");
-			startDate = parseInt(startDate);
-			endDate = parseInt(endDate);
-			var numDays = (endDate - startDate) / 100;
+			HTML3 = "</div></div></div></div></div>",
+			startDate = TRIP.parseDate(flight.get("departDate")),
+			returnDate = TRIP.parseDate(flight.get("returnDate")),
+			numDays = TRIP.daydiff(startDate,returnDate);
+
 		$('.deals').append(
   			HTML0 + imgUrl +
-  			HTML1 + flight.get("destLocation") + " for " + numDays + " nights." +
+  			HTML1 + numDays + " nights in " + TRIP.locationString(flight.get("destLocation")) +
   			HTML2 + "$" + flight.get("price") + " leaving " + flight.get("departDate") + HTML3
 	  	);
 	},
