0203a71ed0963e95d1ab8bf14c7c6d5c506ebba8
Pratik Kabra <pratik@kabra.com>
2013-02-04 17:29:32 -0500

    test

--- flightfinder/flightfinder.php
+++ flightfinder/flightfinder.php
@@ -105,6 +105,7 @@ $airportimagestoadd = array();
 foreach($airports as $airport){
 	$imagestopost = array();
 	set_time_limit(30);
+	echo file_get_contents("http://www.kayak.com/h/rss/buzz?code=".$airport."&tm=".date("Ym"));
 	$rss = json_decode(parse("http://www.kayak.com/h/rss/buzz?code=".$airport."&tm=".date("Ym")), true);
 	$rss2 = json_decode(parse("http://www.kayak.com/h/rss/buzz?code=".$airport."&tm=".(date("Ym")+1)), true);
 	
