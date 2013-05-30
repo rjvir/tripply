d97345c537a3ac88d3fae6696fd14972cae7a2e1
Jesse Daugherty <jessedaugherty@gmail.com>
2013-05-30 01:11:01 -0400

    new dropdown yayyyyy

--- js/main.js
+++ js/main.js
@@ -18,10 +18,12 @@ Parse.initialize("DaJkkAOKSFVxqPbI7gyPluuqRWkUgGIgzDzMJhUD", "X2oTRbTAnIRL3VVyI1
 var deal_template_source = $('#deal-template').html(),
 	dropdown_template_source = $('#dropdown-item-template').html(),
 	deal_template = Handlebars.compile(deal_template_source),
-	dropdown_item_template = Handlebars.compile(dropdown_template_source),
+	// dropdown_item_template = Handlebars.compile(dropdown_template_source),
 	imgUrl = "http://www.photo-dictionary.com/photofiles/list/8/667airplane.jpg";
 
 var TRIP = TRIP || {};
+
+window.td = {pages: []};
 
 moment.calendar = {
     lastDay : '[Yesterday]',
@@ -65,7 +67,8 @@ $.extend(TRIP, {
 		query.find({
 			success: function(results) {
 				if (results && results.length > 0)
-					$('#dropdown-button').prepend(results[0].get("city") + ", " + results[0].get("state") + " (" + results[0].get("airport_code") + ") ");
+					$('#city_input').val(results[0].get("city") + ", " + results[0].get("state") + " (" + results[0].get("airport_code") + ") ");
+					$('#city_input').attr("title",results[0].get("city") + ", " + results[0].get("state") + " (" + results[0].get("airport_code") + ") ");
 			},
 			error: function(results) {
 			    alert("Oops! Something went wrong. The site may be experiencing some issues. Please refresh or visit again later.");
@@ -157,13 +160,32 @@ $.extend(TRIP, {
 		query.ascending("city");
 		query.find({
 			success: function(origin_cities) {
+				blahBlah = [];
+				$.each(origin_cities, function() {
+					ac = this.get("airport_code");
+					c = this.get("city")
+					s = this.get("state")
+					img = this.get("imageUrl")
+					var i = {}
+					i.title = ac;
+					i.text = c + ", " + s;
+					i.tags = ac + " " + c + " " + s + " (" + ac + ") ";
+					i.thumb = img;
+					i.loc = "?origin=" + ac + "/";
+					blahBlah.push(i)
+				})
+				window.td.pages = blahBlah;
+				if (!window.addedBLAH) {
+					$('#city_input').tipuedrop();
+					window.addedBLAH = true
+				}
 		  		$.each(origin_cities, function() {
-					var html = dropdown_item_template({
-						airport_code: this.get("airport_code"), 
-						city: this.get("city"),
-						state: this.get("state")
-					});
-					$("#dropdown-cities ul").append(html);
+					// var html = dropdown_item_template({
+					// 	airport_code: this.get("airport_code"), 
+					// 	city: this.get("city"),
+					// 	state: this.get("state")
+					// });
+					// $("#dropdown-cities ul").append(html);
 				});
 		  	},
 		    error: function(error) {
@@ -176,4 +198,92 @@ $.extend(TRIP, {
  TRIP.getDeals();
  TRIP.getCities();
 
+ var input = $('#city_input');
+
+input.focus(function() {
+     $(this).val('');
+}).blur(function() {
+     var el = $(this);
+
+     /* use the elements title attribute to store the 
+        default text - or the new HTML5 standard of using
+        the 'data-' prefix i.e.: data-default="some default" */
+     if(el.val() == '')
+         el.val(el.attr('title'));
+});
+
+ $.fn.tipuedrop = function(options) {
+
+      var set = $.extend( {
+      
+           'show'                   : 5,
+           'speed'                  : 10,
+           'newWindow'              : false,
+           'mode'                   : 'static',
+           'contentLocation'        : 'tipuedrop/tipuedrop_content.json'
+      
+      }, options);
+      
+      return this.each(function() {
+      
+           var tipuedrop_in = {
+                pages: []
+           };
+           $.ajaxSetup({
+                async: false
+           });                 
+                          
+           if (set.mode == 'static')
+           {
+                tipuedrop_in = $.extend({}, window.td);
+                var i = 0;
+           }
+
+           $(this).keyup(function(event)
+           {
+                getTipuedrop($(this));
+           });               
+           
+           function getTipuedrop($obj)
+           {
+                if ($obj.val())
+                {
+                     var out = '';
+                     var c = 0;
+                     for (var i = 0; i < tipuedrop_in.pages.length; i++)
+                     {
+                          var pat = new RegExp($obj.val(), 'i');
+                          if ((tipuedrop_in.pages[i].title.search(pat) != -1 || tipuedrop_in.pages[i].text.search(pat) != -1 || tipuedrop_in.pages[i].tags.search(pat) != -1) && c < set.show)
+                          {
+                               out += '<a href="' + tipuedrop_in.pages[i].loc + '"';
+                               if (set.newWindow)
+                               {
+                                    out += ' target="_blank"';
+                               }
+                               out += '><div class="tipue_drop_item"><div class="tipue_drop_left"><img src="' + tipuedrop_in.pages[i].thumb + '" class="tipue_drop_image"></div><div class="tipue_drop_right"><div class="tipue_drop_right_title">' + tipuedrop_in.pages[i].title + '</div><div class="tipue_drop_right_text">' + tipuedrop_in.pages[i].text + '</div></div><div style="clear: both;"></div></div></a>';
+                               c++;
+                          }
+                     }
+                     if (c == 0)
+                     {
+                          out += '<div class="tipue_drop_no_items">Origin city not found. <a href="mailto:nowcation@umich.edu">Contact us</a> to add it.</div>';     
+                     }
+                                    
+                     $('#tipue_drop_content').html(out);
+                     $('#tipue_drop_content').fadeIn(set.speed);
+                }
+                else
+                {
+                     $('#tipue_drop_content').fadeOut(set.speed);     
+                }
+           }
+           
+           $('html').click(function() {
+                $('#tipue_drop_content').fadeOut(set.speed);
+           });
+      
+      });
+
+ };
+  
 });	
